4c168b54f1444472ecda968be1da960a60be1e2e
simonedimaria <117766011+perrythepwner@users.noreply.github.com>
2024-03-06 18:10:10 +0100

    version compatible with CTF infra

--- challenge/scripts/entrypoint.sh
+++ challenge/scripts/entrypoint.sh
@@ -2,23 +2,21 @@
 
 set -ex
 
-# setup network namespace + vlan + port forwarding to isolate servers
-/bin/bash /root/scripts/setup-netns.sh
-
-# hide processes
-mount -o remount,rw,hidepid=2 /proc
-
 # start sshd
 /usr/sbin/sshd
+
+# set environment variables
+export NAME="recovery"
+export IMAGE=blockchain_${NAME}
+export HOSTNAME="wallet"
+export FLAG="HTB{n0t_y0ur_k3ys_n0t_y0ur_c01n5}"
+export ELECTRS_IP="0.0.0.0"
+export ELECTRS_PORT=50001
+export HANDLER_PORT=8888
 
 # start startup scripts inside the internal network namespace
 for script in /root/startup/*; do
     echo "[*] running $script"
-    ip netns exec internal env \
-    ELECTRS_IP=$ELECTRS_IP \
-    ELECTRS_PORT=$ELECTRS_PORT \
-    HANDLER_PORT=$HANDLER_PORT \
-    FLAG=$FLAG \
     /bin/bash "$script"
 done
 
